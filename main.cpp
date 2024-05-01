94fbcd869707649118c07f4fa22ecad9a50b3713
Davi Azevedo <daviazevedos02@gmail.com>
2024-04-30 22:22:02 -0300

    c -> c++

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <SDL2/SDL.h>
-#include <stdio.h>
+#include <iostream>
 
 #undef main
 
@@ -10,21 +10,21 @@ int main(int argc, char **argv)
 {
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
-        printf("Error: SDL failed to initialize\nSDL Error: '%s'\n", SDL_GetError());
+        std::cerr << "Error: SDL failed to initialize\nSDL Error: '" << SDL_GetError() << "'\n";
         return 1;
     }
 
-    SDL_Window *window = SDL_CreateWindow("SLD test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    SDL_Window *window = SDL_CreateWindow("SDL test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
     if (!window)
     {
-        printf("Error: Failed to open window\nSDL Error: '%s'\n", SDL_GetError());
+        std::cerr << "Error: Failed to open window\nSDL Error: '" << SDL_GetError() << "'\n";
         return 1;
     }
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer)
     {
-        printf("Error: Failed to create renderer\nSDL Error: '%s'\n", SDL_GetError());
+        std::cerr << "Error: Failed to create renderer\nSDL Error: '" << SDL_GetError() << "'\n";
         return 1;
     }
 
@@ -45,11 +45,15 @@ int main(int argc, char **argv)
             }
         }
 
-        SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+        SDL_SetRenderDrawColor(renderer, 25, 150, 50, 255);
         SDL_RenderClear(renderer);
 
         SDL_RenderPresent(renderer);
     }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
